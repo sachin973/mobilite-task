e694d77c224c11dec52a329c76b02e16233d7a4b
sachin-beniwal <sachin@leewayhertz.com>
2022-07-29 19:59:30 +0530

    build

---
+++ build/precache-manifest.fae7fbd49a444f517e817032108417b7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7828a977aa2978aedfc821dbfca368d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "779a47c7c2b980a170b2",
+    "url": "/static/css/2.560b9325.chunk.css"
+  },
+  {
+    "revision": "b1dc4b743d5df87db2ef",
+    "url": "/static/css/main.6cbe45b9.chunk.css"
+  },
+  {
+    "revision": "779a47c7c2b980a170b2",
+    "url": "/static/js/2.b9bd837d.chunk.js"
+  },
+  {
+    "revision": "b1dc4b743d5df87db2ef",
+    "url": "/static/js/main.e947856d.chunk.js"
+  },
+  {
+    "revision": "2c62f668f4fe935b5f77",
+    "url": "/static/js/runtime~main.d112b23e.js"
+  }
+]);
